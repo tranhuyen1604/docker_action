083c2dc050d9da8a95f36f3eb1fe3041265ddd23
Huyen Tran <tranhuyenbk96@gmail.com>
2025-04-15 16:16:14 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["vercel.com", "nextjs.org"],
+  },
 };
-
-export default nextConfig;
